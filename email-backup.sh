ed5a980656d882545fe7c0ef82b785b8603373a1
thrasherht <root@digi.thrasherht.com>
2017-04-16 20:11:37 -0400

    update argument parsing

--- email-backup.sh
+++ email-backup.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 #Backup script to archive sent emails older then a set number of days
+#This is designed to backup emails put into the Archive folder as well as the Sent folder
+
 VERSION=0.1.0
-USAGE="Usage: email-backup.sh [username] [domain] [Older then days]"
+USAGE="Usage: email-backup.sh [domain] [days]"
+
+#Check for no variables
+if [ $# == 0 ] ; then
+    echo $USAGE
+    exit 1;
+fi
+
+#Check which cpanel user owns domain
+USER=`/scripts/whoowns $1`
 
 #Setup variables
 DATE=`date +%F_%H:%M`
 SENT='true'
 INBOX='true'
-DAYS="$3"
-DIR_PATH="/home/$1/mail/$2"
+DAYS="$2"
+DOMAIN="$1"
+DIR_PATH="/home/$USER/mail/$DOMAIN"
 
 #Move the files or copy the files
-TRANSFER_METHOD="copy"
+TRANSFER_METHOD="move"
 
 
 #Check for no variables
@@ -36,13 +48,16 @@ if [ $SENT == true ] ; then
         for i in */;
         do
                 BACKUP_DIR="/backup/email_backup/$DATE/${i}Sent"
-                SENT_DIR="/home/$1/mail/$2/$i.Sent"
+                SENT_DIR="/home/$USER/mail/$DOMAIN/$i.Sent"
                 mkdir -p $BACKUP_DIR
                 echo "Created backup directory at $BACKUP_DIR"
                     if [ $TRANSFER_METHOD == copy ] ; then
                 copy_email $SENT_DIR $BACKUP_DIR
-                    else
+                    elif [ $TRANSFER_METHOD == move ]; then
                 move_email $SENT_DIR $BACKUP_DIR
+		else
+		echo "Invalid transfer method!"
+		exit 1;
                     fi
         done
         popd 1>/dev/null
@@ -53,17 +68,18 @@ if [ $INBOX == true ] ; then
         for i in */;
         do
                 BACKUP_DIR="/backup/email_backup/$DATE/${i}Archive"
-                ARCHIVE_DIR="/home/$1/mail/$2/$i.Archive"
+                ARCHIVE_DIR="/home/$USER/mail/$DOMAIN/$i.Archive"
                 mkdir -p $BACKUP_DIR
                 echo "Created backup directory at $BACKUP_DIR"
                     if [ $TRANSFER_METHOD == copy ] ; then
                 copy_email $ARCHIVE_DIR $BACKUP_DIR
-                    else
+                    elif [ $TRANSFER_METHOD == move ]; then
                 move_email $ARCHIVE_DIR $BACKUP_DIR
+		else
+		echo "Invalid transfer method!"
+		exit 1;
                     fi
         done
         popd 1>/dev/null
 fi
 echo "Backup finished"
-
-
